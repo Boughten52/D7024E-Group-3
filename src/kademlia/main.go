a37fc8c2533ce2c2e3713266b459b5c62efd36e6
Jonatan Stenlund <jonatanstenlund99@gmail.com>
2023-09-14 13:16:47 +0200

    Separated Kademlia and CLI applications

---
+++ src/kademlia/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// TODO: Start Kademlia network/node
+
+	fmt.Println("Kademlia network started")
+
+	for {
+		time.Sleep(10 * time.Second) // Sleep for 10 seconds to avoid high CPU usage
+	}
+}

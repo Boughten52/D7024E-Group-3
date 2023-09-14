388c7d9ae4bac4ab77e210cc5d751c4d7331b0b1
filber-9 <filber-9@student.ltu.se>
2023-09-14 14:53:42 +0200

    Fix syntax

--- src/kademlia/kademlia.go
+++ src/kademlia/kademlia.go
@@ -1,38 +1,37 @@
 package main
 
 import (
-    "crypto/sha1"
-    "fmt"
-    "math/big"
+	"crypto/sha1"
+	"math/big"
 )
 
 type Kademlia struct {
-	  ID          *big.Int
-    DataStore   map[string]string
+	ID        *big.Int
+	DataStore map[string]string
 }
 
 /*
  * Looks up stored data
- * 
+ *
  * Takes:   the key for the object
  * Returns: the object and a bool if the retreval was successful or not
  */
 func (kademlia *Kademlia) LookUpData(key string) (string, bool) {
-    keyHash := hashKey(key)
-    closestNode := node.lookUpContact(keyHash)
-    value, ok := closestNode.DataStore[keyHash.String()]
-    return value, ok
+	keyHash := hashKey(key)
+	closestNode := kademlia.lookUpContact(keyHash)
+	value, ok := closestNode.DataStore[keyHash.String()]
+	return value, ok
 }
 
 /*
  * Stores given data
- * 
+ *
  * Takes:   the key for the object and the value
  */
 func (kademlia *Kademlia) StoreData(key, value string) {
-    keyHash := hashKey(key)
-    closestNode := node.lookUpContact(keyHash)
-    closestNode.DataStore[keyHash.String()] = value
+	keyHash := hashKey(key)
+	closestNode := kademlia.lookUpContact(keyHash)
+	closestNode.DataStore[keyHash.String()] = value
 }
 
 /////////////////////////
@@ -40,11 +39,11 @@ func (kademlia *Kademlia) StoreData(key, value string) {
 /////////////////////////
 
 func hashKey(key string) *big.Int {
-    hash := sha1.Sum([]byte(key))
-    return new(big.Int).SetBytes(hash[:])
+	hash := sha1.Sum([]byte(key))
+	return new(big.Int).SetBytes(hash[:])
 }
 
-func (kademlia *Kademlia) lookUpContact(target *Contact) {
+func (kademlia *Kademlia) lookUpContact(key *big.Int) *Kademlia {
 	// TODO: Network operation to find k closest to given key.
 	// ATM just return current node.
 	return kademlia
